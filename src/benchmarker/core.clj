d8d1f3f98a631fbd25fdc8872b46097198af1580
Nicholas Kariniemi <nicholas.kariniemi@gmail.com>
2014-03-10 14:33:33 +0200

    Rework handling of config

--- src/benchmarker/core.clj
+++ src/benchmarker/core.clj
@@ -21,41 +21,32 @@
                       java.io.PushbackReader.)]
     (edn/read rdr)))
 
-(def config (load-config))
-(def device-props (:device-props config))
-(def output-dir (:output-dir config))
-(def test-apps (:test-apps config))
-
 (conch/programs adb ant)
 
 (defn current-minute-str []
   (time-fmt/unparse (time-fmt/formatter "yyyyMMdd_HHmm" (time/default-time-zone)) (time/now)))
 
-(defn make-log-dir! [project-dir output-dir]
-  (let [project-dir-name (last (s/split project-dir #"/"))
-        test-dir (str output-dir "/" (current-minute-str))
-        log-dir (str test-dir "/" project-dir-name)]
-    (.mkdir (io/file test-dir))
-    (.mkdir (io/file log-dir))
-    log-dir))
+(defn make-test-dir! [output-dir]
+  (let [test-dir-name (str output-dir "/" (current-minute-str))]
+    (.mkdir (io/file test-dir-name))
+    test-dir-name))
 
-(defn uninstall-app [package-name opts]
-  (adb "uninstall" package-name opts))
-
-(defn install-app [opts]
-  (ant "release" opts)
-  (ant "installr" opts))
+(defn make-app-dir! [{:keys [package-name]} test-dir]
+  (let [app-dir-name (last (s/split package-name #"\."))
+        app-dir (str test-dir "/" app-dir-name)]
+    (.mkdir (io/file app-dir))
+    app-dir))
 
 (defn restart-phone [opts]
   (adb "shell" "reboot" opts)
   (adb "wait-for-device")
-  (Thread/sleep 60000))
+  (Thread/sleep 120000))
 
 (defn run-test [{:keys [package-name start-activity]} opts]
   (adb "shell" "am" "start" "-n" (str package-name "/." start-activity) opts)
   (Thread/sleep 20000)
   (adb "shell" "am" "force-stop" package-name opts)
-  (Thread/sleep 5000))
+  (Thread/sleep 1000))
 
 (defn save-logcat [log-dir opts]
   (with-open [logcat-file (io/writer (str log-dir "/" logcat-filename))]
@@ -69,16 +60,16 @@
 (defn get-props [props opts]
   (into {} (map (fn [prop] [prop (get-prop prop opts)]) props)))
 
-(defn save-device-info [log-dir opts]
+(defn save-device-info [device-props info-dir opts]
   (let [props (get-props device-props opts)
         prop-strs (map (fn [[k v]] (str "[" k "]: [" v "]\n")) props)]
-    (with-open [device-info-file (io/writer (str log-dir "/" device-info-filename))]
+    (with-open [device-info-file (io/writer (str info-dir "/" device-info-filename))]
       (doseq [p (sort prop-strs)] (.write device-info-file p)))))
 
-(defn execute-test [app-config]
-  (let [log-dir (make-log-dir! (:project-dir app-config) output-dir)
-        stdout-file (io/file (str log-dir "/" stdout-filename))
-        stderr-file (io/file (str log-dir "/" stderr-filename))
+(defn execute-test [app-config test-dir device-props]
+  (let [app-dir (make-app-dir! app-config test-dir)
+        stdout-file (io/file (str app-dir "/" stdout-filename))
+        stderr-file (io/file (str app-dir "/" stderr-filename))
         num-trials 10]
     (with-open [stdout-writer (io/writer stdout-file)
                 stderr-writer (io/writer stderr-file)]
@@ -86,21 +77,23 @@
                   :out stdout-writer
                   :err stderr-writer}
             flush-logs (fn [] (.flush stdout-writer) (.flush stderr-writer))]
-        (save-device-info log-dir opts)
-        (flush-logs)
-        (uninstall-app (:package-name app-config) opts)
-        (install-app opts)
+        (save-device-info device-props app-dir opts)
         (flush-logs)
         (restart-phone opts)
         (flush-logs)
         (doall (repeatedly num-trials (fn [] (run-test app-config opts) (flush-logs))))
-        (save-logcat log-dir opts)))))
+        (save-logcat app-dir opts)))))
 
-(defn execute-tests []
-  (doseq [app-config test-apps]
-    (execute-test app-config)))
+(defn execute-tests [{:keys [test-apps output-dir device-props]}]
+  (let [test-dir (make-test-dir! output-dir)]
+    (doseq [app-config test-apps]
+      (try (execute-test app-config test-dir device-props)
+           (catch Exception e (println "Exception, aborting test:" (.getMessage e)))))))
 
 (defn -main
   [& args]
-  (execute-tests))
+  (let [config (load-config)]
+    (if (= (first args) "results")
+      (parser/print-test-results config)
+      (execute-tests config))))
 
