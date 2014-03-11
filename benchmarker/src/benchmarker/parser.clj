860abf9e7fb2a972bc9b2bd772ae6f953be7a652
Nicholas Kariniemi <nicholas.kariniemi@gmail.com>
2014-03-11 17:13:37 +0200

    Skip files without log data

--- benchmarker/src/benchmarker/parser.clj
+++ benchmarker/src/benchmarker/parser.clj
@@ -1,4 +1,5 @@
 (ns benchmarker.parser
+  (:import java.io.FileNotFoundException)
   (:require [clojure.java.io :as io]
             [clj-time.core :as time]
             [clj-time.format :as time-fmt]
@@ -13,18 +14,18 @@
   
 
 (defn get-start-end-times [filename start-activity main-activity]
-  (with-open [rdr (io/reader filename)]
-    (let [start-ex (re-pattern (str "START u0.*" start-activity))
-          end-ex (re-pattern (str "Displayed.*" main-activity))
-          lines (line-seq rdr)
-          matching (filter #(or (re-find start-ex %)
-                                (re-find end-ex %)) lines)
-          pairs (partition 2 matching)
-          start-end (map (fn [[start end]] {:start (parse-time start) 
-                                            :end (parse-time end)})
-                         pairs)]
-      (doseq [m matching] (println m))
-      (doall start-end))))
+  (try (with-open [rdr (io/reader filename)]
+         (let [start-ex (re-pattern (str "START u0.*" start-activity))
+               end-ex (re-pattern (str "Displayed.*" main-activity))
+               lines (line-seq rdr)
+               matching (filter #(or (re-find start-ex %)
+                                     (re-find end-ex %)) lines)
+               pairs (partition 2 matching)
+               start-end (map (fn [[start end]] {:start (parse-time start) 
+                                                 :end (parse-time end)})
+                              pairs)]
+           (doall start-end)))
+       (catch FileNotFoundException e nil)))
 
 (defn get-run-time [{:keys [start end]}]
   (let [interval (time/interval start end)
@@ -39,24 +40,23 @@
        (catch ArithmeticException e nil)))
 
 (defn print-file-results [filename {:keys [start-activity main-activity]}]
-  (println "start1:" start-activity)
-  (let [start-end (get-start-end-times filename start-activity main-activity)
-        times (get-run-times start-end)]
-    (println filename)
-    (doseq [t times]
-      (println t))
-    (println "Average:" (average times))))
+  (if-let [start-end (get-start-end-times filename start-activity main-activity)]
+    (let [times (get-run-times start-end)]
+      (println filename)
+      (doseq [t times] (println t))
+      (println "Average:" (average times)))
+    (println "Could not find file" filename)))
 
 (defn print-app-test-results [app-dir app-config]
   (let [logcat-file (str app-dir "/logcat")]
-    (println "app-config:" app-config)
     (print-file-results logcat-file app-config)))
 
 (defn print-test-results [{:keys [test-apps output-dir]}]
   (let [get-app-dir-name #(last (s/split (:package-name %) #"\."))
         output-dir-files (.listFiles (io/file output-dir))
-        test-dirs (filter #(.isDirectory %) output-dir-files)]
-    (doseq [test-dir test-dirs]
+        test-dirs (filter #(.isDirectory %) output-dir-files)
+        sorted-dirs (sort-by #(.getName %) test-dirs)]
+    (doseq [test-dir sorted-dirs]
       (let [test-dir-files (.listFiles (io/file test-dir))
             app-dirs (filter #(.isDirectory %) test-dir-files)]
         (doseq [app-dir app-dirs]
